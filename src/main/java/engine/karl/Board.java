f9d0a3323b019bf59a29ddc280888649082ad0ce
Nick Hausleitner <nick.hausleitner@gmail.com>
2023-12-26 14:38:13 +0100

    Update Board.java
    Mehr Methoden zur Bitmanipulation implementiert, erster Ansatz für getLegalWhitePawnMoves()
    
    Signed-off-by: Nick Hausleitner <nick.hausleitner@gmail.com>

--- src/main/java/engine/karl/Board.java
+++ src/main/java/engine/karl/Board.java
@@ -1,5 +1,8 @@
 package engine.karl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Board {
 
     private long whitePawns;
@@ -14,6 +17,14 @@ public class Board {
     private long blackRooks;
     private long blackQueens;
     private long blackKing;
+
+    private boolean whiteTurns;
+    private int castlingRights;
+    private int enPassantSquares;
+
+    public long getAllPieces(){
+        return whitePawns | whiteKnights | whiteBishops | whiteRooks | whiteQueens | whiteKing | blackPawns | blackKnights | blackBishops | blackRooks | blackQueens | blackKing;
+    }
 
     public long getWhitePawns() {
         return whitePawns;
@@ -83,6 +94,72 @@ public class Board {
         blackQueens = Long.parseLong("0001000000000000000000000000000000000000000000000000000000000000", 2);
         blackKing = Long.parseLong("0000100000000000000000000000000000000000000000000000000000000000", 2);
     }
+
+    private long switchBit(long bits, int position){
+        return bits ^ (1L << position);
+    }
+
+    private boolean checkIfBitIsSet(long bits, int position){
+        return ((bits & (1L << position)) != 0);
+    }
+
+    private long invertAllBits(long bits){
+        return ~bits;
+    }
+
+    private long deleteAllBitsExceptOne(long bits, int position){
+        return bits & (1L << position);
+    }
+
+    private long shiftToLeft(long bits, int shifts){
+        return bits << shifts;
+    }
+
+    private long shiftToRight(long bits, int shifts){
+        return bits >> shifts;
+    }
+
+    private boolean whiteMated(){
+        return false;
+    }
+
+    private boolean blackMated(){
+        return false;
+    }
+
+    private boolean checkWhite(){
+        return false;
+    }
+
+    private boolean checkBlack(){
+        return false;
+    }
+
+    private Set<Long> getLegalWhitePawnMoves(long whitePawns){
+        Set<Long> LegalWhitePawnMoves = new HashSet<>();
+
+        long firstPawn = deleteAllBitsExceptOne(whitePawns, (int) (Math.log((double) Long.highestOneBit(whitePawns)) / Math.log(2)));
+        long allPieces = getAllPieces();
+        boolean startingPosition = false;
+        if (firstPawn >= Math.pow(2, 8) | firstPawn <= Math.pow(2, 15)){
+            //Doppelzug möglich
+            startingPosition = true;
+        }
+
+        long oneMoveForward = shiftToLeft(firstPawn, 8);
+
+
+
+
+
+
+        return LegalWhitePawnMoves;
+    }
+
+    private void GameRunner(){
+
+    }
+
 
 
 

ef3e4bf2af57ac97a499e4ae1fb2726ee3f1f7b6
Nick Hausleitner <nick.hausleitner@gmail.com>
2024-01-01 16:16:52 +0100

    Update Board.java
    getPossibleWhiteRookMoves()
    
    Signed-off-by: Nick Hausleitner <nick.hausleitner@gmail.com>

--- src/main/java/engine/karl/Board.java
+++ src/main/java/engine/karl/Board.java
@@ -1,13 +1,10 @@
 package engine.karl;
 
+import java.util.*;
+
 public class Board {
 
     public long whitePawns=0L,whiteKnights=0L,whiteBishops=0L,whiteRooks=0L,whiteQueens=0L,whiteKing=0L,blackPawns=0L,blackKnights=0L,blackBishops=0L,blackRooks=0L,blackQueens=0L,blackKing=0L;
-
-    private boolean whiteTurns;
-    private int castlingRights;
-    private int enPassantSquares;
-
     public void initiateStandardChess() {
         String[][] chessboard ={
                 {"r","n","b","q","k","b","n","r"},
@@ -86,9 +83,103 @@ public class Board {
 
     }
 
+    public long getAllPieces(){
+        return whitePawns|whiteRooks|whiteKnights|whiteBishops|whiteQueens|whiteKing|
+                blackPawns|blackRooks|blackKnights|blackBishops|blackQueens|blackKing;
+    }
+
+    public long getAllBlackPieces(){
+        return blackPawns|blackRooks|blackKnights|blackBishops|blackQueens|blackKing;
+    }
+
+    public long getAllWhitePieces(){
+        return whitePawns|whiteRooks|whiteKnights|whiteBishops|whiteQueens|whiteKing;
+    }
+
     public long getPossibleWhiteRookMoves(){
 
+        // Beispiel: "00000000 00000000 00000000 00000000 01000000 00000000 00000000 00000000" wird zu
+        //           "01000000 01000000 01000000 01000000 10111111 01000000 01000000 01000000" (theoretisch bei freiem Board)
+        // ABER
+        // 1. Steht eine weiße Figur im Weg?
+        // 2. Steht eine schwarze Figur im Weg?
+
+        // Idee: Alle Türme in einzelne longs zerlegen und einzeln betrachten.
+        long whiteRooksCopy = whiteRooks;
+        List<Long> whiteRooksSplittet = splitLong(whiteRooksCopy);
+
+        List<Long> legalMoves = new ArrayList<>();
+        long setPieces = getAllPieces();
+
+        for (int a = 0; a < whiteRooksSplittet.size(); a++){
+
+            long rook = whiteRooksSplittet.get(a);
+            long leadingZeros = Long.numberOfLeadingZeros(rook);
+            long startingPosition = 63 - leadingZeros;
+            long rowOfStartingPosition = startingPosition / 8;
+            long columnOfStartingPosition = startingPosition % 8;
+
+            // vertikal nach oben
+            for (int i = 1; i <= 7 - rowOfStartingPosition; i++){
+                long target = startingPosition + (8 * i);
+                long rowOfTarget = target / 8;
+                long columnOfTarget = target % 8;
+                if ((target & setPieces) == 1){
+                    if ((target & blackKing) == 1){
+                        legalMoves.add(target);
+                    } else { // !!!!
+                        break;
+                    }
+                } else if ((target & setPieces) == 0) {
+                    legalMoves.add(target);
+                }
+            }
+
+            // vertikal nach unten
+            for (int i = 1; i <=rowOfStartingPosition; i++){
+                long target = startingPosition - (8 * i);
+                long rowOfTarget = target / 8;
+                long columnOfTarget = target % 8;
+                if ((target & setPieces) == 1){
+                    if ((target & blackKing) == 1){
+                        legalMoves.add(target);
+                    } else { // !!!!
+                        break;
+                    }
+                } else if ((target & setPieces) == 0) {
+                    legalMoves.add(target);
+                }
+            }
+
+        }
+
+
+
+
+        long sum = 0L;
+        for (Long l: legalMoves){
+            sum = sum | l;
+        }
+
+        return sum;
     }
+
+    public static List<Long> splitLong(long input) {
+        List<Long> result = new ArrayList<>();
+        long rest = input;
+        while (rest > 0){
+            long a = 1;
+            while (a*2 <= rest){
+                a=a*2;
+            }
+            rest = rest - a;
+            result.add(a);
+        }
+        return result;
+    }
+
+
+
 
 
 }
